445bfc18b71e5d6a6bb23c86f297e90b1e0944cb
Emmanuel Ngwane <ngwanemk@gmail.com>
2015-04-27 11:57:10 +0200

    Changed Assembly Culture of xmisc.foundation.concretes to none

--- solution/xmisc.foundation.concretes/Properties/AssemblyInfo.cs
+++ solution/xmisc.foundation.concretes/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("xmisc.foundation.concretes")]
 [assembly: AssemblyCopyright("Copyright (c) 2015, reexjungle")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("en")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
