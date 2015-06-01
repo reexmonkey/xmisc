032157217cee38a62e96212c933b8fffaf390997
Emmanuel Ngwane <ngwanemk@gmail.com>
2015-06-01 08:02:03 +0200

    Bug fix on FPI

--- solution/xmisc.foundation.concretes/identifiers.cs
+++ solution/xmisc.foundation.concretes/identifiers.cs
@@ -20,6 +20,11 @@ namespace reexjungle.xmisc.foundation.concretes
         public string Author { get; set; }
 
         /// <summary>
+        /// Gets or sets the reference to the standard authority that approved the FPI
+        /// </summary>
+        public string Reference { get; set; }
+
+        /// <summary>
         /// Gets or sets the product class of the FPI
         /// </summary>
         public string Product { get; set; }
@@ -42,18 +47,21 @@ namespace reexjungle.xmisc.foundation.concretes
         /// <param name="description">The description of the FPI</param>
         /// <param name="language">The language of the FPI</param>
         /// <param name="status">The approval status of the FPI</param>
-        public Fpi(ApprovalStatus status, string author, string product, string description, string language)
+        /// <param name="reference">The standard authority that formally approved the FPI</param>
+        public Fpi(ApprovalStatus status, string author, string product, string description, string language, string reference = null)
         {
             if (status == ApprovalStatus.Standard)
-                author.ThrowIfNullOrEmpty("author");
+                reference.ThrowIfNullOrEmpty("reference");
+            Reference = reference;
 
             Status = status;
+
             Author = author;
+            author.ThrowIfNullOrEmpty("author");
 
             product.ThrowIfNullOrEmpty("product");
             Product = product;
 
-            description.ThrowIfNullOrEmpty("description");
             Description = description;
 
             language.ThrowIfNullOrEmpty("language");
@@ -67,8 +75,9 @@ namespace reexjungle.xmisc.foundation.concretes
         /// <param name="product">The product class of the FPI</param>
         /// <param name="description">The description of the FPI</param>
         /// <param name="language">The language of the FPI</param>
-        public Fpi(string author, string product, string description, string language) :
-            this(ApprovalStatus.Standard, author, product, description, language)
+        /// <param name="reference">The standard authority that formally approved the FPI</param>
+        public Fpi(string author, string product, string description, string language, string reference) :
+            this(ApprovalStatus.Standard, author, product, description, language, reference)
         {
         }
 
@@ -78,7 +87,7 @@ namespace reexjungle.xmisc.foundation.concretes
             switch (Status)
             {
                 case ApprovalStatus.Standard:
-                    sb.Append(Author);
+                    sb.Append(Reference);
                     break;
 
                 case ApprovalStatus.None:
@@ -89,9 +98,9 @@ namespace reexjungle.xmisc.foundation.concretes
                     sb.Append("+");
                     break;
             }
-
+            sb.AppendFormat("//{0}", Author);
             sb.AppendFormat("//{0}", Product);
-            sb.AppendFormat("//{0}", Description);
+            if (!string.IsNullOrEmpty(Description)) sb.AppendFormat(" {0}", Description);
             sb.AppendFormat("//{0}", Language);
             return sb.ToString();
         }
@@ -100,7 +109,7 @@ namespace reexjungle.xmisc.foundation.concretes
         {
             if (ReferenceEquals(null, other)) return false;
             if (ReferenceEquals(this, other)) return true;
-            return Status == other.Status && string.Equals(Author, other.Author) && string.Equals(Product, other.Product) && string.Equals(Description, other.Description) && string.Equals(Language, other.Language);
+            return Status == other.Status && string.Equals(Author, other.Author) && string.Equals(Reference, other.Reference) && string.Equals(Product, other.Product) && string.Equals(Description, other.Description) && string.Equals(Language, other.Language);
         }
 
         public override bool Equals(object obj)
@@ -117,6 +126,7 @@ namespace reexjungle.xmisc.foundation.concretes
             {
                 var hashCode = (int)Status;
                 hashCode = (hashCode * 397) ^ (Author != null ? Author.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Reference != null ? Reference.GetHashCode() : 0);
                 hashCode = (hashCode * 397) ^ (Product != null ? Product.GetHashCode() : 0);
                 hashCode = (hashCode * 397) ^ (Description != null ? Description.GetHashCode() : 0);
                 hashCode = (hashCode * 397) ^ (Language != null ? Language.GetHashCode() : 0);
@@ -124,11 +134,23 @@ namespace reexjungle.xmisc.foundation.concretes
             }
         }
 
+        /// <summary>
+        /// The equality operator
+        /// </summary>
+        /// <param name="left">The FPI operand on the left side of the equality operator</param>
+        /// <param name="right">The FPI operand on the right side of the equality operator</param>
+        /// <returns>True, if both FPI instances are equal, otherwise false.</returns>
         public static bool operator ==(Fpi left, Fpi right)
         {
             return Equals(left, right);
         }
 
+        /// <summary>
+        /// The inequality operator
+        /// </summary>
+        /// <param name="left">The FPI operand on the left side of the equality operator</param>
+        /// <param name="right">The FPI operand on the right side of the equality operator</param>
+        /// <returns>True, if both FPI instances are inequal, otherwise false.</returns>
         public static bool operator !=(Fpi left, Fpi right)
         {
             return !Equals(left, right);
