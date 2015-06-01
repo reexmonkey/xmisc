3c74d203f838aae3a40d6b98d1135cbeaacdccc4
Emmanuel Ngwane <ngwanemk@gmail.com>
2015-06-01 08:04:13 +0200

    Change to FPI interface

--- solution/xmisc.foundation.contracts/identifiers.cs
+++ solution/xmisc.foundation.contracts/identifiers.cs
@@ -11,9 +11,14 @@
         ApprovalStatus Status { get; set; }
 
         /// <summary>
-        ///  Gets or sets the owner of the FPI, if it is approved by a standard authority e.g. ISO
+        ///  Gets or sets the owner of the FPI
         /// </summary>
         string Author { get; set; }
+
+        /// <summary>
+        /// Gets or sets the reference to the standard authority that approved the FPI
+        /// </summary>
+        string Reference { get; set; }
     }
 
     /// <summary>
