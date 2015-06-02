719d6bcbf1fe93747699d373acc8e7fb84c5a2c9
Emmanuel Ngwane <ngwanemk@gmail.com>
2015-06-02 23:42:01 +0200

    Updated generator definition

--- solution/xmisc.infrastructure.contracts/generators.cs
+++ solution/xmisc.infrastructure.contracts/generators.cs
@@ -3,7 +3,25 @@
 namespace reexjungle.xmisc.infrastructure.contracts
 {
     /// <summary>
-    /// Specifies a contract for generating keys
+    /// Specifies a contract for generating content
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    public interface IGenerator<out TValue>
+    {
+        /// <summary>
+        /// Produces the next content
+        /// </summary>
+        /// <returns></returns>
+        TValue GetNext();
+
+        /// <summary>
+        /// Resets the content generator
+        /// </summary>
+        void Reset();
+    }
+
+    /// <summary>
+    /// Specifies a contract for generating unique identifiers
     /// </summary>
     /// <typeparam name="TKey">The given type of key</typeparam>
     public interface IKeyGenerator<TKey>
@@ -13,7 +31,7 @@ namespace reexjungle.xmisc.infrastructure.contracts
         /// Produces the next key
         /// </summary>
         /// <returns>The next available key</returns>
-        TKey GetNextKey();
+        TKey GetNext();
 
         /// <summary>
         /// Recaptures a key for re-use purposes
