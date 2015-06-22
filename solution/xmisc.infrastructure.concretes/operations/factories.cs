6c2d0d09f37a4b86d126f7257eea4a0988ae6e6a
Emmanuel Ngwane <ngwanemk@gmail.com>
2015-06-22 19:51:16 +0200

    Implemented contracts that specify generic factory behavior

---
+++ solution/xmisc.infrastructure.concretes/operations/factories.cs
@@ -0,0 +1,90 @@
+﻿using reexjungle.xmisc.foundation.concretes;
+using reexjungle.xmisc.foundation.contracts;
+using reexjungle.xmisc.infrastructure.contracts;
+using System;
+
+namespace reexjungle.xmisc.infrastructure.concretes.operations
+{
+    /// <summary>
+    ///Represents a factory that creates instances of a given type
+    /// </summary>
+    public class Factory : IFactory
+    {
+        /// <summary>
+        /// Creates an instance from a  given type
+        /// </summary>
+        /// <typeparam name="TValue">The type of the instance to be created</typeparam>
+        /// <returns>The created instance if successful; the default value of TValue</returns>
+        public TValue Create<TValue>() where TValue : class, new()
+        {
+            try
+            {
+                return Activator.CreateInstance<TValue>();
+            }
+            catch (MissingMethodException)
+            {
+                return default(TValue);
+            }
+        }
+
+        public TValue Create<TValue>(params object[] args) where TValue : class, new()
+        {
+            try
+            {
+                return (TValue)Activator.CreateInstance(typeof(TValue), args);
+            }
+            catch (MissingMethodException)
+            {
+                return default(TValue);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Represents a factory creating keyed instances of a given type.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    public class Factory<TKey> : IFactory<TKey> where TKey : IEquatable<TKey>
+    {
+        private readonly IKeyGenerator<TKey> keyGenerator;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="keyGenerator">Key generator to provide keys for the created instances</param>
+        public Factory(IKeyGenerator<TKey> keyGenerator)
+        {
+            this.keyGenerator.ThrowIfNull();
+            this.keyGenerator = keyGenerator;
+        }
+
+        /// <summary>
+        /// Creates a keyed instance from a given type.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the instance to be created.</typeparam>
+        /// <returns>The created instance if successful; otherwise the default value of TValue.</returns>
+        public TValue Create<TValue>() where TValue : class, IContainsKey<TKey>
+        {
+            try
+            {
+                return (TValue)Activator.CreateInstance(typeof(TValue), keyGenerator.GetNext());
+            }
+            catch (MissingMethodException)
+            {
+                return default(TValue);
+            }
+        }
+
+        public TValue Create<TValue>(params object[] otherArgs) where TValue : class, IContainsKey<TKey>
+        {
+            try
+            {
+                return (TValue)Activator.CreateInstance(typeof(TValue), keyGenerator.GetNext(), otherArgs);
+            }
+            catch (MissingMethodException)
+            {
+                return default(TValue);
+            }
+        }
+    }
+}
