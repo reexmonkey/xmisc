63eea4a6b1dc0ae347997b85846603cda97fba9b
Emmanuel Ngwane <ngwanemk@gmail.com>
2015-06-26 08:44:40 +0200

    Finished implementation of CantorPair and ElegantPair mappings

--- solution/xmisc.foundation.concretes/math.cs
+++ solution/xmisc.foundation.concretes/math.cs
@@ -421,7 +421,7 @@ namespace reexjungle.xmisc.foundation.concretes
         /// <param name="x">The first number of the pair.</param>
         /// <param name="y">The second number of the pair.</param>
         /// <returns>The unique number resulting from the mapping.</returns>
-        public static uint CantorPairMap(ushort x, ushort y)
+        public static uint CantorPairMap(this ushort x, ushort y)
         {
             return (uint)((((x + y) * (x + y + 1)) / 2) + y);
         }
@@ -432,7 +432,7 @@ namespace reexjungle.xmisc.foundation.concretes
         /// <param name="x">The first number of the pair.</param>
         /// <param name="y">The second number of the pair.</param>
         /// <returns>The unique number resulting from the mapping.</returns>
-        public static ulong CantorPairMap(uint x, uint y)
+        public static ulong CantorPairMap(this uint x, uint y)
         {
             return (((x + y) * (x + y + 1)) / (2)) + y;
         }
@@ -443,17 +443,17 @@ namespace reexjungle.xmisc.foundation.concretes
         /// <param name="a">The first number of the pair.</param>
         /// <param name="b">The second number of the pair.</param>
         /// <returns>The unique number resulting from the mapping.</returns>
-        public static BigInteger CantorPairMap(ulong a, ulong b)
+        public static BigInteger CantorPairMap(this ulong a, ulong b)
         {
             return (((a + b) * (a + b + 1)) / (2)) + b;
         }
 
         /// <summary>
-        ///
+        /// Finds the Cantor pair, which were mapped to a number.
         /// </summary>
-        /// <param name="z"></param>
-        /// <returns></returns>
-        public static Tuple<BigInteger, BigInteger> InverseCantorPairMap(BigInteger z)
+        /// <param name="z">The number that resulted from a Cantor map.</param>
+        /// <returns>The original pair of numbers that were used to produce the Cantor pair. </returns>
+        public static Tuple<BigInteger, BigInteger> InverseCantorPairMap(this BigInteger z)
         {
             var temp = new BigInteger(8) * z;
             var root = Math.Exp(BigInteger.Log(temp) * 0.5) + 1;
@@ -463,16 +463,26 @@ namespace reexjungle.xmisc.foundation.concretes
             return Tuple.Create(x, y);
         }
 
-        public static Tuple<ulong, ulong> InverseCantorPairMap(ulong value)
+        /// <summary>
+        /// Finds the Cantor pair, which were mapped to a number.
+        /// </summary>
+        /// <param name="z">The number that resulted from a Cantor map.</param>
+        /// <returns>The original pair of numbers that were used to produce the Cantor pair. </returns>
+        public static Tuple<ulong, ulong> InverseCantorPairMap(this ulong z)
         {
-            var root = (ulong)((Math.Sqrt((8 * value) + 1) - 1) / 2);
-            var a = ((root * root) + root) / 2;
-            var c = value - a;
-            var b = root - c;
-            return Tuple.Create(a, b);
+            var root = (ulong)((Math.Sqrt((8 * z) + 1) - 1) / 2);
+            var w = ((root * root) + root) / 2;
+            var x = z - w;
+            var y = root - x;
+            return Tuple.Create(x, y);
         }
 
-        public static Tuple<uint, uint> InverseCantorPairMap(uint z)
+        /// <summary>
+        /// Finds the Cantor pair, which were mapped to a number.
+        /// </summary>
+        /// <param name="z">The number that resulted from a Cantor map.</param>
+        /// <returns>The original pair of numbers that were used to produce the Cantor pair. </returns>
+        public static Tuple<uint, uint> InverseCantorPairMap(this uint z)
         {
             var root = (uint)((Math.Sqrt((8 * z) + 1) - 1) / 2);
             var w = ((root * root) + root) / 2;
@@ -518,6 +528,78 @@ namespace reexjungle.xmisc.foundation.concretes
             return x > y
                 ? (x * x) + x + y
                 : x + (y * y);
+        }
+
+        /// <summary>
+        /// Finds the Elegant pair, which was mapped to a number.
+        /// </summary>
+        /// <param name="z">The number that resulted from an Elegant Pair mapping.</param>
+        /// <returns>The original pair of numbers that were used to produce the Cantor pair. </returns>
+        public static Tuple<BigInteger, BigInteger> InverseElegantPairMap(this BigInteger z)
+        {
+            var root = Math.Exp(BigInteger.Log(z) * 0.5) + 1;
+            var rootsqr = root * root;
+
+            if (root < rootsqr)
+            {
+                var x = BigInteger.Subtract(z, new BigInteger(rootsqr));
+                var y = new BigInteger(root);
+                return Tuple.Create(x, y);
+            }
+            else
+            {
+                var x = new BigInteger(root);
+                var y = BigInteger.Subtract(z, BigInteger.Subtract(new BigInteger(rootsqr), x));
+                return Tuple.Create(x, y);
+            }
+        }
+
+        /// <summary>
+        /// Finds the Elegant pair, which was mapped to a number.
+        /// </summary>
+        /// <param name="z">The number that resulted from the Elegant mapping.</param>
+        /// <returns>The original pair of numbers used to produce the Elegant pair. </returns>
+        public static Tuple<ulong, ulong> InverseElegantPairMap(this ulong z)
+        {
+            var root = Math.Sqrt(z);
+            var rootsqr = root * root;
+
+            if (root < rootsqr)
+            {
+                var x = (ulong)(z - rootsqr);
+                var y = (ulong)root;
+                return Tuple.Create(x, y);
+            }
+            else
+            {
+                var x = (ulong)root;
+                var y = (ulong)(z - rootsqr - root);
+                return Tuple.Create(x, y);
+            }
+        }
+
+        /// <summary>
+        /// Finds the Elegant pair, which was mapped to a number.
+        /// </summary>
+        /// <param name="z">The number that resulted from the Elegant mapping.</param>
+        /// <returns>The original pair of numbers used to produce the Elegant pair.</returns>
+        public static Tuple<uint, uint> InverseElegantPairMap(this uint z)
+        {
+            var root = Math.Sqrt(z);
+            var rootsqr = root * root;
+
+            if (root < rootsqr)
+            {
+                var x = (uint)(z - rootsqr);
+                var y = (uint)root;
+                return Tuple.Create(x, y);
+            }
+            else
+            {
+                var x = (uint)root;
+                var y = (uint)(z - rootsqr - root);
+                return Tuple.Create(x, y);
+            }
         }
 
         #endregion other functions
