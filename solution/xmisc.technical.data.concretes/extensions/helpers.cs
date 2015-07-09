42544ecf7b9458efc92cd0f24c5805421c399ffa
Emmanuel Ngwane <ngwanemk@gmail.com>
2015-07-09 08:30:15 +0200

    Added new projects: xmisc.technical.data.*

---
+++ solution/xmisc.technical.data.concretes/extensions/helpers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace reexjungle.technical.data.concretes.extensions
+{
+    /// <summary>
+    /// Represents conjunctions to connect ORMlite queries
+    /// </summary>
+    public enum Conjunctor
+    {
+        /// <summary>
+        /// Keyword for the AND conjunction
+        /// </summary>
+        And,
+
+        /// <summary>
+        /// Keyword for the OR conjunction
+        /// </summary>
+        Or
+    }
+
+    /// <summary>
+    /// Represents the type of ORMlite join.
+    /// </summary>
+    [Flags]
+    public enum JoinMode
+    {
+        /// <summary>
+        /// Inner Join
+        /// </summary>
+        Inner = 0x0001,
+
+        /// <summary>
+        /// Outer Join
+        /// </summary>
+        Outer = 0x0002,
+
+        /// <summary>
+        /// Left Join
+        /// </summary>
+        Left = 0x0004,
+
+        /// <summary>
+        /// Right join
+        /// </summary>
+        Right = 0x0008,
+
+        /// <summary>
+        /// Full Join
+        /// </summary>
+        Full = 0x0010
+    }
+}
