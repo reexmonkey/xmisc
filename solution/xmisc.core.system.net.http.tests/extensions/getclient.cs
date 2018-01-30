469a47fe44f01efb10cee54355ff027968d4b7ad
reexmonkey <ngwanemk@gmail.com>
2018-01-30 18:02:29 +0100

    Added new test for http client extensions

---
+++ solution/xmisc.core.system.net.http.tests/extensions/getclient.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using reexmonkey.xmisc.core.system.net.http.extensions;
+using xmisc.core.system.net.http.tests.fixtures;
+using Xunit;
+using reexmonkey.xmisc.backbone.io.jil.serializers;
+using System.Threading.Tasks;
+
+namespace xmisc.core.system.net.http.tests.extensions
+{
+    public class HttpGetClientExtensionTests
+    {
+
+        [Fact]
+        public void TestJsonClientGet()
+        {
+            //arrange
+            var address = Fixture.Client.BaseAddress.Combine("questions".ToUri());
+
+            //act
+            var polls = Fixture.Client.Get<List<Poll>>(address, Fixture.JilTextSerializer);
+
+            //assert
+            Assert.NotEmpty(polls);
+        }
+
+        [Fact]
+        public void TestJsonStreamClientGet()
+        {
+            //arrange
+            var address = Fixture.Client.BaseAddress.Combine("questions".ToUri());
+
+            //act
+            var polls = Fixture.Client.Get<List<Poll>>(address, Fixture.JilStreamSerializer);
+
+            //assert
+            Assert.NotEmpty(polls);
+        }
+
+        [Fact]
+        public async Task TestJsonClientGetAsync()
+        {
+            //arrange
+            var address = Fixture.Client.BaseAddress.Combine("questions".ToUri());
+            
+            //act
+            var polls = await Fixture.Client.GetAsync<List<Poll>>(address, Fixture.JilTextSerializer);
+
+            //assert
+            Assert.NotEmpty(polls);
+        }
+
+        [Fact]
+        public async Task TestJsonStreamClientGetAsync()
+        {
+            //arrange
+            var address = Fixture.Client.BaseAddress.Combine("questions".ToUri());
+
+            //act
+            var polls = await Fixture.Client.GetAsync<List<Poll>>(address, Fixture.JilStreamSerializer);
+
+            //assert
+            Assert.NotEmpty(polls);
+        }
+    }
+}
---
+++ solution/xmisc.core.system.net.http.tests/fixtures/fixture.cs
@@ -0,0 +1,87 @@
+﻿using reexmonkey.xmisc.backbone.io.jil.serializers;
+using reexmonkey.xmisc.backbone.io.messagepack.serializers;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace xmisc.core.system.net.http.tests.fixtures
+{
+    [DataContract]
+    public class Answer
+    {
+        [DataMember(Name = "choice")]
+        public string Choice { get; set; }
+
+        [DataMember(Name = "votes")]
+        public int Votes { get; set; }
+    }
+
+    [DataContract]
+    public class Poll
+    {
+        [DataMember(Name ="question")]
+        public string Question { get; set; }
+
+        [DataMember(Name = "published_at")]
+        public string PublishedAt { get; set; }
+
+        [DataMember(Name = "choices")]
+        public List<Answer> Choices { get; set; }
+    }
+
+    public static class Fixture
+    {
+        public static string BaseUrl = "http://polls.apiblueprint.org/";
+
+        public static HttpClient Client = new HttpClient()
+        {
+            BaseAddress = BaseUrl.ToUri()
+        };
+
+        public static JilTextSerializer JilTextSerializer = new JilTextSerializer();
+
+        public static JilStreamSerializer JilStreamSerializer = new JilStreamSerializer(new UTF8Encoding(false), 16*1024);
+
+        public static Uri ToUri(this string url) => new Uri(url, UriKind.RelativeOrAbsolute);
+
+        public static Uri Combine(this Uri @base, Uri relative) => new Uri(@base, relative);
+
+        public static List<Poll> Polls = new List<Poll>
+        {
+            new Poll
+            {
+                Question = "Favourite programming language?",
+                PublishedAt = "2015-08-05T08:40:51.620Z",
+                Choices = new List<Answer>
+                {
+                    new Answer
+                    {
+                        Choice = "Swift",
+                        Votes = 2048
+                    },
+
+                    new Answer
+                    {
+                        Choice = "Python",
+                        Votes = 1024
+                    },
+
+                    new Answer
+                    {
+                        Choice = "Objective-C",
+                        Votes = 512
+                    },
+
+                    new Answer
+                    {
+                        Choice = "Ruby",
+                        Votes = 256
+                    },
+
+                }
+            }
+        };
+    }
+}
