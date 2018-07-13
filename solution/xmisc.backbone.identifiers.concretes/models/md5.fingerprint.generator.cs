a314fb747549898db9b2d7828cfe9a530f915ee0
reexmonkey <ngwanemk@gmail.com>
2018-07-13 17:34:02 +0200

    Removed unused class variable.

--- solution/xmisc.backbone.identifiers.concretes/models/md5.fingerprint.generator.cs
+++ solution/xmisc.backbone.identifiers.concretes/models/md5.fingerprint.generator.cs
@@ -1,13 +1,10 @@
 using reexmonkey.xmisc.backbone.identifiers.contracts.models;
-using reexmonkey.xmisc.core.cryptography.extensions;
 using reexmonkey.xmisc.core.io.serializers;
 using System;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace reexmonkey.xmisc.backbone.identifiers.concretes.models
 {
-
     /// <summary>
     /// Provides a generator of MD5-based fingerprints for specified objects.
     /// </summary>
@@ -15,7 +12,6 @@ namespace reexmonkey.xmisc.backbone.identifiers.concretes.models
     {
         private readonly byte[] namespaceId;
         private readonly BinarySerializerBase serializer;
-        private readonly HashAlgorithm cipher;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Md5FingerprintGenerator"/>.
@@ -32,7 +28,7 @@ namespace reexmonkey.xmisc.backbone.identifiers.concretes.models
         }
 
         /// <summary>
-        /// Gets the default fingerprint for all types of objects. 
+        /// Gets the default fingerprint for all types of objects.
         /// </summary>
         /// <returns>The default fingerprint for all types of objects.</returns>
         public Md5Guid GetNullFingerprint() => Md5Guid.Empty;
@@ -45,5 +41,4 @@ namespace reexmonkey.xmisc.backbone.identifiers.concretes.models
         /// <returns>The fingerprint that uniquely identifies or pseudo-identifies the specified <paramref name="model"/>.</returns>
         public Md5Guid GetFingerprint<TModel>(TModel model) => Md5Guid.NewGuid(namespaceId, serializer.Serialize(model));
     }
-
 }
