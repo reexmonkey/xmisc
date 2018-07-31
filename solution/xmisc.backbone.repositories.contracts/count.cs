3789ce6e27b969eae41c4402e1bba14b38652be5
reexmonkey <ngwanemk@gmail.com>
2018-07-31 15:49:37 +0200

    Add a new repository interface to deal with counting of elements from the data store

---
+++ solution/xmisc.backbone.repositories.contracts/count.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace reexmonkey.xmisc.backbone.repositories.contracts
+{
+    /// <summary>
+    /// Specifies a repository that counts the data models of a data store.
+    /// </summary>
+    /// <typeparam name="TModel">The type of data model to count.</typeparam>
+    public interface ICountRepository<TModel>
+    {
+        /// <summary>
+        /// Counts all data models in the data store.
+        /// </summary>
+        /// <returns>The total number of data models in the data store.</returns>
+        long Count();
+
+        /// <summary>
+        /// Counts the specified data models in the data store that satisfy the given predicate.
+        /// </summary>
+        /// <param name="predicate">The condition for a data model that when evaluated to true, includes the data model in the count.</param>
+        /// <returns>The total number of data models that satisfy the given predicate.</returns>
+        long Count(Expression<Func<TModel, bool>> predicate);
+
+        /// <summary>
+        /// Asynchronously counts all data models in the data store.
+        /// </summary>
+        /// <param name="token">Propagates the notification that the asynchronous operation should be cancelled.</param>
+        /// <returns>The total number of data models in the data store.</returns>
+        Task<long> CountAsync(CancellationToken token = default(CancellationToken));
+
+        /// <summary>
+        /// Asynchronously counts the specified data models in the data store that satisfy the given predicate.
+        /// </summary>
+        /// <param name="predicate">The condition for a data model that when evaluated to true, includes the data model in the count.</param>
+        /// <param name="token">Propagates the notification that the asynchronous operation should be cancelled.</param>
+        /// <returns>The total number of data models that satisfy the given predicate.</returns>
+        Task<long> CountAsync(Expression<Func<TModel, bool>> predicate, CancellationToken token = default(CancellationToken));
+    }
+}
