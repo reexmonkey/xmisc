0b284f05a641cc31422985cf3633cfadea714ddb
reexmonkey <ngwanemk@gmail.com>
2018-08-01 15:25:56 +0200

    Added expression extensions and renamed namespaces

--- solution/xmisc.core.linq/extensions/expressions.cs
+++ solution/xmisc.core.linq/extensions/expressions.cs
@@ -1,15 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using reexmonkey.xmisc.core.linq.extensions;
 
-namespace reexmonkey.xmisc.core.linq.infrastructure
+namespace reexmonkey.xmisc.core.linq.extensions
 {
     /// <summary>
-    /// Provides extended funtionality to lambda epxressions
+    /// Extends the features of the <see cref="Expression"/> class.
     /// </summary>
-    public static class LambdaExpressionExtensions
+    public static class ExpressionExtensions
     {
         /// <summary>
         /// Produces the name of a member in a lambda expression
@@ -24,17 +23,23 @@ namespace reexmonkey.xmisc.core.linq.infrastructure
                 {
                     case ExpressionType.Parameter:
                         return ((ParameterExpression)e).Name;
+
                     case ExpressionType.MemberAccess:
                         return ((MemberExpression)e).Member.Name;
+
                     case ExpressionType.Call:
                         return ((MethodCallExpression)e).Method.Name;
+
                     case ExpressionType.Convert:
                     case ExpressionType.ConvertChecked:
                         return Selector(((UnaryExpression)e).Operand);
+
                     case ExpressionType.Invoke:
                         return Selector(((InvocationExpression)e).Expression);
+
                     case ExpressionType.ArrayLength:
                         return "Length";
+
                     default:
                         throw new Exception("not a proper member selector");
                 }
@@ -56,25 +61,93 @@ namespace reexmonkey.xmisc.core.linq.infrastructure
                 {
                     case ExpressionType.Parameter:
                         return ((ParameterExpression)e).Name.AsSingleton();
+
                     case ExpressionType.MemberAccess:
                         return ((MemberExpression)e).Member.Name.AsSingleton();
+
                     case ExpressionType.New:
                         return ((NewExpression)e).Members.Select(x => x.Name);
+
                     case ExpressionType.Call:
                         return ((MethodCallExpression)e).Method.Name.AsSingleton();
+
                     case ExpressionType.Convert:
                     case ExpressionType.ConvertChecked:
                         return Selector(((UnaryExpression)e).Operand);
+
                     case ExpressionType.Invoke:
                         return Selector(((InvocationExpression)e).Expression);
+
                     case ExpressionType.ArrayLength:
                         return "Length".AsSingleton();
+
                     default:
                         throw new Exception("not a proper member selector");
                 }
             }
 
             return Selector(expression.Body);
+        }
+
+        /// <summary>
+        /// Combines two expression that encapsulate lambda functions <see cref="Func{T, TResult}"/> using the "OR" logic.
+        /// </summary>
+        /// <typeparam name="T">The type specified in the lambda functions.</typeparam>
+        /// <param name="this">The first exprsession to combine.</param>
+        /// <param name="other">The second expression to combine.</param>
+        /// <returns>The "OR" combination of the expressions.</returns>
+        public static Expression<Func<T, bool>> OrElse<T>(this Expression<Func<T, bool>> @this, Expression<Func<T, bool>> other)
+        {
+            var parameter = Expression.Parameter(typeof(T));
+
+            var leftVisitor = new ReplaceExpressionVisitor(@this.Parameters[0], parameter);
+            var left = leftVisitor.Visit(@this.Body);
+
+            var rightVisitor = new ReplaceExpressionVisitor(other.Parameters[0], parameter);
+            var right = rightVisitor.Visit(other.Body);
+
+            return Expression.Lambda<Func<T, bool>>(
+                Expression.OrElse(left, right), parameter);
+        }
+
+        /// <summary>
+        /// Combines two expression that encapsulate lambda functions <see cref="Func{T, TResult}"/> using the "AND" logic.
+        /// </summary>
+        /// <typeparam name="T">The type specified in the lambda functions.</typeparam>
+        /// <param name="this">The first exprsession to combine.</param>
+        /// <param name="other">The second expression to combine.</param>
+        /// <returns>The "AND" combination of the expressions.</returns>
+        public static Expression<Func<T, bool>> AndAlso<T>(this Expression<Func<T, bool>> @this, Expression<Func<T, bool>> other)
+        {
+            var parameter = Expression.Parameter(typeof(T));
+
+            var leftVisitor = new ReplaceExpressionVisitor(@this.Parameters[0], parameter);
+            var left = leftVisitor.Visit(@this.Body);
+
+            var rightVisitor = new ReplaceExpressionVisitor(other.Parameters[0], parameter);
+            var right = rightVisitor.Visit(other.Body);
+
+            return Expression.Lambda<Func<T, bool>>(
+                Expression.AndAlso(left, right), parameter);
+        }
+
+        private class ReplaceExpressionVisitor : ExpressionVisitor
+        {
+            private readonly Expression oldValue;
+            private readonly Expression newValue;
+
+            public ReplaceExpressionVisitor(Expression oldValue, Expression newValue)
+            {
+                this.oldValue = oldValue;
+                this.newValue = newValue;
+            }
+
+            public override Expression Visit(Expression node)
+            {
+                if (node == oldValue)
+                    return newValue;
+                return base.Visit(node);
+            }
         }
     }
 }
