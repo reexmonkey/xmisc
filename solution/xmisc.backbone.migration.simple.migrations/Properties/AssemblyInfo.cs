59dacd9f5a64acde5ae2fe190a841faccfd4b66a
reexmonkey <ngwanemk@gmail.com>
2018-08-09 19:21:15 +0200

    Added migrations project that extends features of Simple.Migrations

---
+++ solution/xmisc.backbone.migration.simple.migrations/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xmisc.backbone.migration.simple.migrations")]
+[assembly: AssemblyDescription("Utilities for the Simple.Migrations library.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("reexmonkey")]
+[assembly: AssemblyProduct("xmisc.backbone.migration.simple.migrations")]
+[assembly: AssemblyCopyright("Copyright (c) 2015-2018, reexmonkey")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: AssemblyVersion(
+    ThisAssembly.Git.BaseVersion.Major + "." +
+    ThisAssembly.Git.BaseVersion.Minor + "." +
+    ThisAssembly.Git.BaseVersion.Patch)]
+[assembly: AssemblyFileVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.SemVer.Patch)]
+[assembly: AssemblyInformationalVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.SemVer.Patch +
+    ThisAssembly.Git.SemVer.DashLabel)]
