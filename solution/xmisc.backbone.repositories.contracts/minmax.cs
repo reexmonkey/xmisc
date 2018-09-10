04e17cb951d199c58b060b775bb713795111d75b
reexmonkey <ngwanemk@gmail.com>
2018-09-10 10:56:03 +0200

    Refactored to make interface deal only with minimum or maximum key, in order to make intent for implementation clearer.

--- solution/xmisc.backbone.repositories.contracts/minmax.cs
+++ solution/xmisc.backbone.repositories.contracts/minmax.cs
@@ -6,83 +6,67 @@ using System.Threading.Tasks;
 namespace reexmonkey.xmisc.backbone.repositories.contracts
 {
     /// <summary>
-    /// Specifies a repository that returns a minimum or maximum scalar result after the evaluation of a scalar expression.
+    /// Specifies a repository that returns the samllest or largest unique identifier of data models in a data store.
     /// </summary>
-    /// <typeparam name="TModel">The type of data model, for which the scalar expression is evaluated.</typeparam>
-    public interface IMinMaxRepository<TModel>
+    /// <typeparam name="TKey">The type of the key that uniquely identifies data models in the data store.</typeparam>
+    /// <typeparam name="TModel">The type of data model in the data store.</typeparam>
+    public interface IMinMaxKeyRepository<TKey, TModel>
+        where TKey : IEquatable<TKey>, IComparable, IComparable<TKey>
     {
         /// <summary>
-        /// Evaluates an internal scalar expression and returns the smallest value.
-        /// <para /> The internal scalar expression is to be defined at the implementation of this method.
+        /// Determines the smallest unique identifier (key) for a set of data models that satisfy the specified predicate.
         /// </summary>
-        /// <typeparam name="T">The type of scalar result.</typeparam>
-        /// <returns>The smallest value from the internal evaluation of the scalar expression.</returns>
-        T Min<T>() where T : IEquatable<T>, IComparable, IComparable<T>;
+        /// <returns>The smallest unique identifier (key) of data models.</returns>
+        TKey MinKey();
 
         /// <summary>
-        /// Evaluates an internal scalar expression and returns the smallest value for a set of data models that satisfy the specified predicate.
+        /// Determines the smallest unique identifier (key) for a set of data models that satisfy the specified predicate.
         /// </summary>
-        /// <typeparam name="T">The type of scalar result.</typeparam>
-        /// <param name="predicate">Filters the set of data models for which the scalar expression shall be evaluated.</param>
-        /// <returns>The smallest value from the internal evaluation of the scalar expression.</returns>
-        T Min<T>(Expression<Func<TModel, bool>> predicate)
-            where T : IEquatable<T>, IComparable, IComparable<T>;
+        /// <param name="predicate">Filters the set of data models for which the smallest unique identifier (key) shall be identified.</param>
+        /// <returns>The smallest unique identifier (key) of specified data models.</returns>
+        TKey MinKey(Expression<Func<TModel, bool>> predicate);
 
         /// <summary>
-        /// Evaluates an internal scalar expression and returns the largest value.
-        /// <para /> The internal scalar expression is to be defined at the implementation of this method.
+        /// Determines the largest unique identifier (key) for a set of data models that satisfy the specified predicate.
         /// </summary>
-        /// <typeparam name="T">The type of scalar result.</typeparam>
-        /// <returns>The largest value from the internal evaluation of the scalar expression.</returns>
-        T Max<T>() where T : IEquatable<T>, IComparable, IComparable<T>;
+        /// <returns>The largest unique identifier (key) of data models.</returns>
+        TKey MaxKey();
 
         /// <summary>
-        /// Evaluates an internal scalar expression and returns the largest value for a set of data models that satisfy the specified predicate.
+        /// Determines the largest unique identifier (key) for a set of data models that satisfy the specified predicate.
         /// </summary>
-        /// <typeparam name="T">The type of scalar result.</typeparam>
-        /// <param name="predicate">Filters the set of data models for which the scalar expression shall be evaluated.</param>
-        /// <returns>The largest value from the internal evaluation of the scalar expression.</returns>
-        T Max<T>(Expression<Func<TModel, bool>> predicate)
-            where T : IEquatable<T>, IComparable, IComparable<T>;
+        /// <param name="predicate">Filters the set of data models for which the largest unique identifier (key) shall be identified.</param>
+        /// <returns>The largest unique identifier (key) of specified data models.</returns>
+        TKey MaxKey(Expression<Func<TModel, bool>> predicate);
 
         /// <summary>
-        /// Evaluates an internal scalar expression asynchronously and returns the smallest value.
-        /// <para /> The internal scalar expression is to be defined at the implementation of this method.
+        /// Determines the smallest unique identifier (key) asynchronously for a set of data models that satisfy the specified predicate.
         /// </summary>
-        /// <typeparam name="T">The type of scalar result.</typeparam>
         /// <param name="token">Propagates the notification that the asynchronous operation should be cancelled.</param>
-        /// <returns>A promise to return the smallest value from the internal evaluation of the scalar expression.</returns>
-        Task<T> MinAsync<T>(CancellationToken token = default(CancellationToken))
-            where T : IEquatable<T>, IComparable, IComparable<T>;
+        /// <returns>A promise to return the smallest unique identifier (key) of data models.</returns>
+        Task MinAsync(CancellationToken token = default(CancellationToken));
 
         /// <summary>
-        /// Evaluates an internal scalar expression asynchronously and returns the smallest value for a set of data models that satisfy the specified predicate.
+        /// Determines the smallest unique identifier (key) asynchronously for a set of data models that satisfy the specified predicate.
         /// </summary>
-        /// <typeparam name="T">The type of scalar result.</typeparam>
-        /// <param name="predicate">Filters the set of data models for which the scalar expression shall be evaluated.</param>
+        /// <param name="predicate">Filters the set of data models for which the smallest unique identifier (key) shall be identified.</param>
         /// <param name="token">Propagates the notification that the asynchronous operation should be cancelled.</param>
-        /// <returns>A promise to return the smallest value from the internal evaluation of the scalar expression.</returns>
-        Task<T> MinAsync<T>(Expression<Func<TModel, bool>> predicate, CancellationToken token = default(CancellationToken))
-            where T : IEquatable<T>, IComparable, IComparable<T>;
+        /// <returns>The smallest unique identifier (key) of specified data models.</returns>
+        Task MinAsync(Expression<Func<TModel, bool>> predicate, CancellationToken token = default(CancellationToken));
 
         /// <summary>
-        /// Evaluates an internal scalar expression asynchronously and returns the largest value.
-        /// <para /> The internal scalar expression is to be defined at the implementation of this method.
+        /// Determines the largest unique identifier (key) asynchronously for a set of data models that satisfy the specified predicate.
         /// </summary>
-        /// <typeparam name="T">The type of scalar result.</typeparam>
         /// <param name="token">Propagates the notification that the asynchronous operation should be cancelled.</param>
-        /// <returns>A promise to return the largest value from the internal evaluation of the scalar expression.</returns>
-        Task<T> MaxAsync<T>(CancellationToken token = default(CancellationToken))
-            where T : IEquatable<T>, IComparable, IComparable<T>;
+        /// <returns>A promise to return the largest unique identifier (key) of data models.</returns>
+        Task MaxAsync(CancellationToken token = default(CancellationToken));
 
         /// <summary>
-        /// Evaluates an internal scalar expression asynchronously and returns the largest value for a set of data models that satisfy the specified predicate.
+        /// Determines the largest unique identifier (key) asynchronously for a set of data models that satisfy the specified predicate.
         /// </summary>
-        /// <typeparam name="T">The type of scalar result.</typeparam>
-        /// <param name="predicate">Filters the set of data models for which the scalar expression shall be evaluated.</param>
+        /// <param name="predicate">Filters the set of data models for which the largest unique identifier (key) shall be identified.</param>
         /// <param name="token">Propagates the notification that the asynchronous operation should be cancelled.</param>
-        /// <returns>A promise to return the largest value from the internal evaluation of the scalar expression.</returns>
-        Task<T> MaxAsync<T>(Expression<Func<TModel, bool>> predicate, CancellationToken token = default(CancellationToken))
-            where T : IEquatable<T>, IComparable, IComparable<T>;
+        /// <returns>The largest unique identifier (key) of specified data models.</returns>
+        Task MaxAsync(Expression<Func<TModel, bool>> predicate, CancellationToken token = default(CancellationToken));
     }
 }
