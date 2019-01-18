9e4cf40d922652eb5727dd5c59d8fe0f4090aaf9
reexmonkey <ngwanemk@gmail.com>
2019-01-18 14:15:01 +0100

    Refactoring

--- solution/xmisc.core.system.net/extensions/network.cs
+++ solution/xmisc.core.system.net/extensions/network.cs
@@ -21,13 +21,13 @@ namespace reexmonkey.xmisc.core.system.net.extensions
 
         private static IPAddress[] GetIPAddresses(string hostNameOrAddress) => Dns.GetHostAddresses(hostNameOrAddress);
 
-        private static string GetIPAddress(string remoteHost, int port, AddressFamily family)
+        private static IPAddress GetIPAddress(string remoteHost, int port, AddressFamily family)
         {
             using (var socket = new Socket(family, SocketType.Dgram, 0))
             {
                 socket.Connect(remoteHost, port);
                 var endpoint = socket.LocalEndPoint as IPEndPoint;
-                return endpoint.Address.ToString();
+                return endpoint.Address;
             }
         }
 
@@ -37,14 +37,14 @@ namespace reexmonkey.xmisc.core.system.net.extensions
             return Dns.GetHostAddressesAsync(hostNameOrAddress);
         }
 
-        private async static Task<string> GetIPAddressAsync(string remoteHost, int port, AddressFamily family, CancellationToken token = default(CancellationToken))
+        private async static Task<IPAddress> GetIPAddressAsync(string remoteHost, int port, AddressFamily family, CancellationToken token = default(CancellationToken))
         {
             using (var socket = new Socket(family, SocketType.Dgram, 0))
             {
                 await socket.ConnectAsync(remoteHost, port).ConfigureAwait(false);
                 token.ThrowIfCancellationRequested();
                 var endpoint = socket.LocalEndPoint as IPEndPoint;
-                return endpoint.Address.ToString();
+                return endpoint.Address;
             }
         }
 
@@ -86,43 +86,43 @@ namespace reexmonkey.xmisc.core.system.net.extensions
 
         public static bool NetworkAvailable() => NetworkInterface.GetIsNetworkAvailable();
 
-        public static IPAddress[] GetIPv4Addreses(string hostNameOrAddress)
+        public static IPAddress GetLocalIPv4Address(string remoteHost, int port, Func<bool> networkAvailableFunc)
+            => networkAvailableFunc() ? GetIPAddress(remoteHost, port, AddressFamily.InterNetwork) : LoopbackIPv4.AsIPAddress();
+
+        public static IPAddress GetLocalIPv6Address(string remoteHost, int port, Func<bool> networkAvailableFunc)
+            => networkAvailableFunc() ? GetIPAddress(remoteHost, port, AddressFamily.InterNetworkV6) : LoopbackIPv6.AsIPAddress();
+
+        public static IPAddress[] GetLocalIPv4Addresses(string hostNameOrAddress)
             => GetIPAddresses(hostNameOrAddress).Where(x => x.AddressFamily == AddressFamily.InterNetwork).ToArray();
 
-        public static IPAddress[] GetIPv6Addreses(string hostNameOrAddress)
+        public static IPAddress[] GetLocalIPv6Addresses(string hostNameOrAddress)
             => GetIPAddresses(hostNameOrAddress).Where(x => x.AddressFamily == AddressFamily.InterNetworkV6).ToArray();
-
-        public static string GetIPv4Address(string remoteHost, int port, Func<bool> networkAvailableFunc)
-            => networkAvailableFunc() ? GetIPAddress(remoteHost, port, AddressFamily.InterNetwork) : LoopbackIPv4;
-
-        public static string GetIPv6Address(string remoteHost, int port, Func<bool> networkAvailableFunc)
-            => networkAvailableFunc() ? GetIPAddress(remoteHost, port, AddressFamily.InterNetworkV6) : LoopbackIPv6;
 
         public static string ToString(this IPEndPoint endpoint, char delimiter) => endpoint.ToString().Replace(':', delimiter);
 
         public static string Format(this int port, string protocol) => $"{port.ToString()}/{protocol}";
 
-        public async static Task<string> GetIPv4AddressAsync(string remoteHost, int port, Func<bool> networkAvailableFunc, CancellationToken token = default(CancellationToken))
+        public async static Task<IPAddress> GetLocalIPv4AddressAsync(string remoteHost, int port, Func<bool> networkAvailableFunc, CancellationToken token = default(CancellationToken))
         {
             return networkAvailableFunc()
                 ? await GetIPAddressAsync(remoteHost, port, AddressFamily.InterNetwork, token).ConfigureAwait(false)
-                : LoopbackIPv4;
+                : LoopbackIPv4.AsIPAddress();
         }
 
-        public async static Task<string> GetIPv6AddressAsync(string remoteHost, int port, Func<bool> networkAvailableFunc, CancellationToken token = default(CancellationToken))
+        public async static Task<IPAddress> GetLocalIPv6AddressAsync(string remoteHost, int port, Func<bool> networkAvailableFunc, CancellationToken token = default(CancellationToken))
         {
             return networkAvailableFunc()
                 ? await GetIPAddressAsync(remoteHost, port, AddressFamily.InterNetwork, token).ConfigureAwait(false)
-                : LoopbackIPv4;
+                : LoopbackIPv4.AsIPAddress();
         }
 
-        public async static Task<IPAddress[]> GetIPv4AddresesAsync(string hostNameOrAddress, CancellationToken token = default(CancellationToken))
+        public async static Task<IPAddress[]> GetLocalIPv4AddressesAsync(string hostNameOrAddress, CancellationToken token = default(CancellationToken))
         {
             var addresses = await GetIPAddressesAsync(hostNameOrAddress, token).ConfigureAwait(false);
             return addresses.Where(x => x.AddressFamily == AddressFamily.InterNetwork).ToArray();
         }
 
-        public static async Task<IPAddress[]> GetIPv6AddresesAsync(string hostNameOrAddress, CancellationToken token = default(CancellationToken))
+        public static async Task<IPAddress[]> GetLocalIPv6AddressesAsync(string hostNameOrAddress, CancellationToken token = default(CancellationToken))
         {
             var addresses = await GetIPAddressesAsync(hostNameOrAddress, token).ConfigureAwait(false);
             return addresses.Where(x => x.AddressFamily == AddressFamily.InterNetworkV6).ToArray();
