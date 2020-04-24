291809096e1b9010e8220c984373d40f97995249
Emmanuel Makebe Ngwane <emmanuel.makebe.ngwane@pwc.com>
2020-04-24 13:09:58 +0200

    removed ReadBytes methods due to detected potential memory leaks

--- solution/xmisc.core.io/extensions/stream.cs
+++ solution/xmisc.core.io/extensions/stream.cs
@@ -12,25 +12,6 @@ namespace reexmonkey.xmisc.core.io.extensions
     public static class StreamExtensions
     {
         /// <summary>
-        /// Reads bytes from stream.
-        /// </summary>
-        /// <param name="stream">The stream to read.</param>
-        /// <param name="buffersize">The size of the buffer during the read process.</param>
-        /// <returns>Bytes read from the stream.</returns>
-        public static byte[] ReadBytes(this Stream stream, int buffersize = 4096)
-        {
-            if (stream is MemoryStream memorystream) return memorystream.ToArray();
-
-            var buffer = new byte[buffersize];
-            using (var mstream = new MemoryStream())
-            {
-                int read;
-                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0) mstream.Write(buffer, 0, read);
-                return mstream.ToArray();
-            }
-        }
-
-        /// <summary>
         /// Gets the checksum of a <paramref name="stream"/> object.
         /// </summary>
         /// <param name="stream">The stream whose integerity is checked.</param>
@@ -41,29 +22,9 @@ namespace reexmonkey.xmisc.core.io.extensions
         public static string GetChecksum(this Stream stream, HashAlgorithm algorithm)
         {
             var hash = algorithm.ComputeHash(stream);
-            return hash.Any() ? BitConverter.ToString(hash) : string.Empty;
-        }
-
-        /// <summary>
-        /// Reads bytes from stream in an asynchronous operation.
-        /// </summary>
-        /// <param name="stream">The stream to read.</param>
-        /// <param name="buffersize">The size of the buffer during the read process.</param>
-        /// <returns>The promise of bytes read from the stream.</returns>
-        public static async Task<byte[]> ReadBytesAsync(this Stream stream, int buffersize = 4096)
-        {
-            if (stream is MemoryStream memorystream) return await Task.FromResult(memorystream.ToArray());
-
-            var buffer = new byte[buffersize];
-            using (var mstream = new MemoryStream())
-            {
-                int read;
-                while ((read = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-                {
-                    await mstream.WriteAsync(buffer, 0, read);
-                }
-                return await Task.FromResult(mstream.ToArray());
-            }
+            return hash.Any()
+                ? BitConverter.ToString(hash)
+                : string.Empty;
         }
 
         /// <summary>
