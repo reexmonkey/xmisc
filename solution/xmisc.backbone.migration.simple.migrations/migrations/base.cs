8e86d9bcb3782d4212f387d8455a4008ccb680cf
Emmanuel Makebe Ngwane <emmanuel.makebe.ngwane@pwc.com>
2020-04-25 00:50:49 +0200

    Updated project details and dependencies

--- solution/xmisc.backbone.migration.simple.migrations/migrations/base.cs
+++ solution/xmisc.backbone.migration.simple.migrations/migrations/base.cs
@@ -1,5 +1,8 @@
 ﻿using SimpleMigrations;
+using System;
+using System.Collections.Generic;
 using System.Data.Common;
+using System.Linq;
 using System.Transactions;
 
 namespace reexmonkey.xmisc.backbone.migration.simple.migrations.migrations
@@ -20,7 +23,7 @@ namespace reexmonkey.xmisc.backbone.migration.simple.migrations.migrations
         protected IMigrationLogger Logger { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether calls to <see cref="Execute(string, int?)"/> should be run inside of a transaction.
+        /// Gets or sets a value indicating whether calls to <see cref="Execute(string, Func{string, DbConnection, DbCommand}, IEnumerable{DbParameter}, int?)"/> should be run inside of a transaction.
         /// </summary>
         /// <remarks>
         /// If this is false, <see cref="Transaction"/> will not be set.
@@ -41,13 +44,16 @@ namespace reexmonkey.xmisc.backbone.migration.simple.migrations.migrations
         /// Execute and log an SQL query (which returns no data)
         /// </summary>
         /// <param name="sql">SQL to execute</param>
+        /// <param name="cmdFunc">The lambda function to create the SQL command object.</param>
+        /// <param name="parameters">Parameters for the SQL command.</param>
         /// <param name="commandTimeout">The command timeout to use (in seconds), or null to use the default from your ADO.NET provider</param>
-        public void Execute(string sql, int? commandTimeout = null)
+        public void Execute(string sql, Func<string, DbConnection, DbCommand> cmdFunc, IEnumerable<DbParameter> parameters,  int? commandTimeout = null)
         {
             if (Logger != null) Logger.LogSql(sql);
-            using (var command = Connection.CreateCommand())
+            using (var command =  cmdFunc(sql, Connection))
             {
-                command.CommandText = sql;
+                command.Parameters.Clear();
+                command.Parameters.AddRange(parameters.ToArray());
                 if (commandTimeout.HasValue) command.CommandTimeout = commandTimeout.Value;
                 command.ExecuteNonQuery();
             }
